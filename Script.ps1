0d48930493a9f8fc36315a4ac81c3bf28fa0f93c
Skyfay <76796007+Skyfay@users.noreply.github.com>
2022-03-29 08:23:01 +0200

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -11,23 +11,28 @@ $howmany = Read-Host "Wie viele Netzwerk Schnittstellen moechtest du zum NIC-TEA
 
 if ($howmany -eq 2) {
 cls
+while($true) {
 Get-NetAdapter
 $network1 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $network2 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
-break
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
+break
 }
 else {
+    cls
+    echo "Hmm, das hat nicht funktioniert. Bitte achte auf deine Rechtschreibung und versuche es erneut"
+}
 }
 }
 if ($howmany -eq 3) {
 cls
+while($true) {
 Get-NetAdapter
 $network1 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $network2 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
@@ -35,18 +40,22 @@ $network3 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2","$network3" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
-break
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
+break
 }
 else {
+    cls
+    echo "Hmm, das hat nicht funktioniert. Bitte achte auf deine Rechtschreibung und versuche es erneut"
+}
 }
 }
 if ($howmany -eq 4) {
 cls
 Get-NetAdapter
+while($true) {
 $network1 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $network2 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $network3 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
@@ -54,34 +63,36 @@ $network4 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2","$network3","$network4" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
-break
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
+break
 }
 else {
+    cls
+    echo "Hmm, das hat nicht funktioniert. Bitte achte auf deine Rechtschreibung und versuche es erneut"
 }
 }
 }
-while($true) {
+}
 if ($start -eq 2) {
+while($true) {
 Get-NetAdapter
 $nicteaming = Read-Host "Welches NIC-Teaming moechtest du loeschen?"
 Remove-VMSwitch $nicteaming
 if ($error.clear()) {
-break
 echo "Das NIC-Teaming wurde erfolgreich geloescht, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
+break
 }
 else {
      cls
-     echo "Das war wohl nix! Bitte gib einen richtigen Nic-Teaming Adapter an..."
+     Write-Error -Message "Das war wohl nix! Bitte gib einen richtigen Nic-Teaming Adapter an..."
 }
 }
 }
 else {
     cls
     echo "Gibt bitte einen in der Frage genannten Wert ein!"
-}
 }
