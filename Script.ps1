31155d0e95dba9919a1788da208f2f6ad496c7ea
Skyfay <76796007+Skyfay@users.noreply.github.com>
2022-03-29 07:50:32 +0200

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -17,11 +17,11 @@ $network2 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
+break
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
-break
 }
 else {
 }
@@ -35,11 +35,11 @@ $network3 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2","$network3" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
+break
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
-break
 }
 else {
 }
@@ -54,11 +54,11 @@ $network4 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2","$network3","$network4" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
+break
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
-break
 }
 else {
 }
@@ -70,18 +70,18 @@ Get-NetAdapter
 $nicteaming = Read-Host "Welches NIC-Teaming moechtest du loeschen?"
 Remove-VMSwitch $nicteaming
 if ($error.clear()) {
+break
 echo "Das NIC-Teaming wurde erfolgreich geloescht, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
 Start-Sleep -s 6
-break
 }
 else {
      cls
      echo "Das war wohl nix! Bitte gib einen richtigen Nic-Teaming Adapter an..."
 }
 }
+}
 else {
     cls
     echo "Gibt bitte einen in der Frage genannten Wert ein!"
-}
 }
 }
