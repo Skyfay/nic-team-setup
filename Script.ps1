dcf2d44300932801b8db8cc0a70bdd476933b522
Skyfay <76796007+Skyfay@users.noreply.github.com>
2022-03-29 09:02:10 +0200

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -18,6 +18,7 @@ $network2 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
 New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2" -EnableEmbeddedTeaming $true
 Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
+echo $error.Count
 Start-Sleep -s 10
 cls
 if ($error.clear()) {
@@ -62,8 +63,8 @@ $network2 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $network3 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $network4 = Read-Host "Wie lautet die erste Netzwerk Schnittstelle?"
 $nicname = Read-Host "Wie soll das Nic Teaming heissen?"
-New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2","$network3","$network4" -EnableEmbeddedTeaming $true | out-null
-Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic | out-null
+New-VMSwitch -Name $nicname -NetAdapterName "$network1","$network2","$network3","$network4" -EnableEmbeddedTeaming $true
+Set-VMSwitchTeam -Name $nicname -LoadBalancingAlgorithm Dynamic
 cls
 if ($error.clear()) {
 echo "Das NIC-Teaming wurde erfolgreich durchgefuehrt, Glueckwunsch! Dieses Fenster schliesst sich automatisch."
